d25f7ba8ba326e0b628446cbd0778f3834e0e8fa
James <wong.james2003@gmail.com>
2024-08-03 16:42:19 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
-import Josephine from './Pages/Josephine';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
-        <Route path='josephine' element={<Josephine />} />
       </Routes>
       </BrowserRouter>
   );
